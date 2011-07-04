ef80ed0df32cb610f5f3c46924251210048592ed
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2011-07-04 17:54:05 +0300

    moving back to 0062...

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -3,6 +3,14 @@
 //--------------------------------------------------------------
 void testApp::setup(){
 
+    box2d.init();
+    box2d.setGravity(0, 1);
+    // box2d.createFloor();
+    box2d.createBounds();
+    box2d.checkBounds(true);
+    box2d.setFPS(30.0);
+
+
 }
 
 //--------------------------------------------------------------
--- src/testApp.h
+++ src/testApp.h
@@ -1,7 +1,6 @@
 #ifndef _TEST_APP
 #define _TEST_APP
 
-
 #include "ofMain.h"
 
 class testApp : public ofBaseApp{
@@ -18,6 +17,8 @@ class testApp : public ofBaseApp{
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
+
+        ofxBox2d    box2d;
 
 };
 
