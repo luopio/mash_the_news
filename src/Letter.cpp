53d0e899a61a7da4c6a2d13b6480dfb3e6ed7f85
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2011-07-04 19:34:22 +0300

    string char stufff

--- src/Letter.cpp
+++ src/Letter.cpp
@@ -2,19 +2,18 @@
 
 Letter::Letter(const char let, ofTrueTypeFont &f)
 {
-    letter = string(&let);
+    letter = string(1, let);
     font = &f;
     row = ofRandom(0, 10);
     col = ofRandom(0, 10);
     tex = new ofxFBOTexture();
     tex->allocate(FONT_SIZE + 3, FONT_SIZE + 3, false);
     tex->begin();
-    ofFill();
-    ofSetColor(255, 0, 0);
-    ofRect(0, 0, 100, 100);
-    ofSetColor(0, 0, 255);
-    font->drawString(letter, 0, FONT_SIZE);
-    cout << letter << endl;
+        // ofFill();
+        // ofSetColor(255, 0, 0);
+        // ofRect(0, 0, 100, 100);
+        ofSetColor(0, 0, 255);
+        font->drawString(letter, 0, FONT_SIZE);
     tex->end();
 }
 
