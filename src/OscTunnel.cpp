3ecf347511f44bbe061949ade671e0701303cd4f
Jarse <jarse@aarre.(none)>
2011-07-25 18:11:11 +0300

    osc viritysta

--- src/OscTunnel.cpp
+++ src/OscTunnel.cpp
@@ -33,88 +33,140 @@ OscTunnel::~OscTunnel()
 void OscTunnel::update() {
     while( receiver.hasWaitingMessages() )
 	{
-        // get the next message
-		ofxOscMessage m;
-		receiver.getNextMessage( &m );
 
-		// check for mouse moved message
-		if ( m.getAddress() == "/testmessage" )
-		{
-			// both the arguments are int32's
-			cout << "Test Received!!!" << endl;
-			//screen->messages.push_back( new Message(string("kokko")));
-		}
-		else if ( m.getAddress() == "/sms" )
-		{
-			// both the arguments are int32's
-			cout << "SMS: " << m.getArgAsString( 0 ) << endl;
-			//screen->messages.push_back( new Message(m.getArgAsString(0)));
+	    try {
 
-		} else if ( m.getAddress() == "/oscmidi" )
-		{
-            if (m.getArgAsString(0)== "noteoff") {
-                cout << "oscmidi noteoff: ch: " << m.getArgAsInt32(1) << " note: " << m.getArgAsInt32(2) << " velocity: " << m.getArgAsInt32(3) << endl;
-                 if (m.getArgAsInt32(2)==40) {
-                    screen->hilightMessage(-10);
-                }
-            } else if (m.getArgAsString(0)== "noteon") {
-                cout << "oscmidi noteon:  ch: " << m.getArgAsInt32(1) << " note: " << m.getArgAsInt32(2) << " velocity: " << m.getArgAsInt32(3) << endl;
-                switch (m.getArgAsInt32(2)) {
-                    case 40:
-                        screen->hilightMessage(0);
-                        break;
-                    case 39:
-                        screen->currentEngine->debug = !screen->currentEngine->debug;
-                        break;
-                    case 38:
-                        kDebug = !kDebug;
-                        break;
-                }
-            } else if (m.getArgAsString(0)== "cc") {
+            // get the next message
+            ofxOscMessage m;
+            receiver.getNextMessage( &m );
 
-                switch (m.getArgAsInt32(2)) {
-                    case 1:
-                        screen->currentEngine->strength = m.getArgAsInt32(3) / 60.0;
-                        cout << screen->currentEngine->strength << " is new strength!" << endl;
+            // check for mouse moved message
+            if ( m.getAddress() == "/testmessage" )
+            {
+                // both the arguments are int32's
+                cout << "Test Received!!!" << endl;
+                //screen->messages.push_back( new Message(string("kokko")));
+            }
+            else if ( m.getAddress() == "/sms" )
+            {
+                // both the arguments are int32's
+                cout << "SMS: " << m.getArgAsString( 0 ) << endl;
+                //screen->messages.push_back( new Message(m.getArgAsString(0)));
+
+            } else if ( m.getAddress() == "/oscmidi" )
+            {
+                if (m.getArgAsString(0)== "noteoff") {
+                    cout << "oscmidi noteoff: ch: " << m.getArgAsInt32(1) << " note: " << m.getArgAsInt32(2) << " velocity: " << m.getArgAsInt32(3) << endl;
+                    if (m.getArgAsInt32(2)==40) {
+                        screen->hilightMessage(-10);
+                    }
+                } else if (m.getArgAsString(0)== "noteon") {
+                    cout << "oscmidi noteon:  ch: " << m.getArgAsInt32(1) << " note: " << m.getArgAsInt32(2) << " velocity: " << m.getArgAsInt32(3) << endl;
+                    switch (m.getArgAsInt32(2)) {
+                        case 40:
+                            screen->hilightMessage(0);
+                            break;
+                        case 39:
+                            screen->currentEngine->debug = !screen->currentEngine->debug;
+                            break;
+                        case 38:
+                            kDebug = !kDebug;
+                            break;
+                    }
+                } else if (m.getArgAsString(0)== "cc") {
+
+
+                    switch (m.getArgAsInt32(1)) {
+
+                        /* Channel 3 is for spaghetti!  */
+                        case 0:
+                            switch (m.getArgAsInt32(2)) {
+                                case 1:
+                                    screen->currentEngine->strength = m.getArgAsInt32(3) / 60.0;
+                                    cout << screen->currentEngine->strength << " is new strength!" << endl;
+                                    break;
+                                case 2:
+                                    screen->currentEngine->damping = m.getArgAsInt32(3) / 120.0;
+                                    cout << screen->currentEngine->damping << " is new damping!" << endl;
+                                    break;
+                                case 3:
+                                    screen->currentEngine->minDis = m.getArgAsInt32(3) * 10.0;
+                                    cout << screen->currentEngine->minDis << " is new minimum distance!" << endl;
+                                    break;
+                                case 5:
+                                    updateBgString(m.getArgAsInt32(3)+33);
+                                    //cout << kFarThreshold << " is new far threshold value" << endl;
+                                    break;
+
+                            }
+                        case 1:
+
+                        case 2:
+
+                        /* Channel 3 is for calibrating stuff */
+                        case 3:
+                            switch (m.getArgAsInt32(2)) {
+                                case 0:
+
+                                break;
+                                case 6:
+                                    if (kinect != NULL) {
+                                        kinect->setCameraTiltAngle((m.getArgAsInt32(3)/(127 / 60.0)) -30);
+                                        cout << (m.getArgAsInt32(3)/(127 / 60.0)) -30 << " is new near threshold value" << endl;
+                                    }
+                                    break;
+
+                                case 7:
+                                    if (kinect != NULL) {
+                                        kThreshold = 254 - (m.getArgAsInt32(3) * 2);
+                                        cout << kThreshold << " is new near threshold value" << endl;
+                                    }
+                                    break;
+                                case 8:
+                                    if (kinect != NULL) {
+                                        kFarThreshold = 254 - (m.getArgAsInt32(3) * 2);
+                                        cout << kFarThreshold << " is new far threshold value" << endl;
+                                    }
+                                break;
+
+                            }
                         break;
-                    case 2:
-                        screen->currentEngine->damping = m.getArgAsInt32(3) / 120.0;
-                        cout << screen->currentEngine->damping << " is new damping!" << endl;
-                        break;
-                    case 3:
-                        screen->currentEngine->minDis = m.getArgAsInt32(3) * 10.0;
-                        cout << screen->currentEngine->minDis << " is new minimum distance!" << endl;
-                        break;
-                    case 5:
-                        updateBgString(m.getArgAsInt32(3)+33);
-                        //cout << kFarThreshold << " is new far threshold value" << endl;
-                        break;
-                    case 6:
-                        if (kinect != NULL) {
-                            kinect->setCameraTiltAngle((m.getArgAsInt32(3)/(127 / 60.0)) -30);
-                            cout << (m.getArgAsInt32(3)/(127 / 60.0)) -30 << " is new near threshold value" << endl;
+                    }
+
+
+                    switch (m.getArgAsInt32(2)) {
+                        case 1:
+                            screen->currentEngine->strength = m.getArgAsInt32(3) / 60.0;
+                            cout << screen->currentEngine->strength << " is new strength!" << endl;
+                            break;
+                        case 2:
+                            screen->currentEngine->damping = m.getArgAsInt32(3) / 120.0;
+                            cout << screen->currentEngine->damping << " is new damping!" << endl;
+                            break;
+                        case 3:
+                            screen->currentEngine->minDis = m.getArgAsInt32(3) * 10.0;
+                            cout << screen->currentEngine->minDis << " is new minimum distance!" << endl;
+                            break;
+                        case 5:
+                            updateBgString(m.getArgAsInt32(3)+33);
+                            //cout << kFarThreshold << " is new far threshold value" << endl;
+                            break;
+
+                        default:
+                            cout << "oscmidi cc:      ch: " << m.getArgAsInt32(1) << " param: " << m.getArgAsInt32(2) << " value: " << m.getArgAsInt32(3) << endl;
                         }
-                        break;
-                    case 7:
-                        kThreshold = 254 - (m.getArgAsInt32(3) * 2);
-                        cout << kThreshold << " is new near threshold value" << endl;
-                        break;
-                    case 8:
-                        kFarThreshold = 254 - (m.getArgAsInt32(3) * 2);
-                        cout << kFarThreshold << " is new far threshold value" << endl;
-                        break;
-                    default:
-                        cout << "oscmidi cc:      ch: " << m.getArgAsInt32(1) << " param: " << m.getArgAsInt32(2) << " value: " << m.getArgAsInt32(3) << endl;
-                }
-            }
+                    }
 
-            else {
-                cout << "oscmidi message received!!!" << endl;
+                    else {
+                        cout << "oscmidi message received!!!" << endl;
+                    }
+                } else {
+                    cout << "unknown osc message received!!!" << endl;
+                }
+            } catch ( ... ) {
+                cout << "problematic osc message gone to dev/null!" << endl;
             }
-		} else {
-            cout << "unknown osc message received!!!" << endl;
-		}
-	}
+        }
 }
 
 void OscTunnel::addOscListener(string address, void (*callback) (ofxOscMessage)) {
